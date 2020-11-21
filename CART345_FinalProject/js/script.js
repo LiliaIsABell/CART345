84baa65feed54a55cf3f5d564cb03761605b1e5d
LiliaIsABell <54867489+LiliaIsABell@users.noreply.github.com>
2020-11-21 15:05:19 -0500

    Allow activity to reset by clicking

--- CART345_FinalProject/js/script.js
+++ CART345_FinalProject/js/script.js
@@ -12,26 +12,6 @@ CART345 Final Project
 // Array
 let wordObjects = [];
 
-// // Word objects
-// // All words are here
-// let wordI;
-// let wordAm;
-// let wordSorry;
-// let wordWas;
-// let wordWrong;
-// let wordLove;
-// let wordYou;
-// let wordNeed;
-// let wordHelp;
-// let wordPlease;
-// let wordStop;
-// let wordWant;
-// let wordThis;
-// let wordIn;
-// let wordMy;
-// let wordOpinion;
-// let wordDisagree;
-
 // div
 let $droppableBox;
 let $clearScreen;
@@ -87,109 +67,117 @@ function setup() {
   // Clicking allows for project to start
   $titleScreen.click(start);
 
-  // Start
+  //List of Words
   //
-  // When the user clicks the screen
-  // the project starts
-  function start() {
-    // Setup annyang
-    if (annyang) {
-      annyang.addCommands(speech);
-      annyang.start();
+  // I
+  wordObjects.push(new Word(100, 0, 1, 0, "I", 150, 0));
+  // AM
+  wordObjects.push(new Word(500, -100, -1, 0, "AM", 1050, 1));
+  // // SORRY
+  wordObjects.push(new Word(990, 50, 1, 0, "SORRY", 800, 2));
+  // WAS
+  wordObjects.push(new Word(105, 142, -5, 0, "WAS", 1050, 3));
+  // WRONG
+  wordObjects.push(new Word(900, -187, 5, 0, "WRONG", 3000, 4));
+  // LOVE
+  wordObjects.push(new Word(0, -50, -1, 0, "LOVE", 800, 5));
+  // YOU
+  wordObjects.push(new Word(1000, 0, 5, 0, "YOU", 1050, 6));
+  // NEED
+  wordObjects.push(new Word(-8, 45, -3, 0, "NEED", 1050, 7));
+  // HELP
+  wordObjects.push(new Word(580, 0, 3, 0, "HELP", 3000, 8));
+  // PLEASE
+  wordObjects.push(new Word(700, 160, -3, 0, "PLEASE", 3000, 9));
+  // STOP
+  wordObjects.push(new Word(-8, -187, 1, 0, "STOP", 800, 10));
+  // WANT
+  wordObjects.push(new Word(500, -140, -3, 0, "WANT", 1050, 11));
+  // THIS
+  wordObjects.push(new Word(300, 100, 1, 0, "THIS", 1050, 12));
+  // IN
+  wordObjects.push(new Word(1380, 142, -5, 0, "IN", 1050, 13));
+  // MY
+  wordObjects.push(new Word(300, -187, 5, 0, "MY", 1050, 14));
+  // OPINION
+  wordObjects.push(new Word(800, -50, -3, 0, "OPINION", 3000, 15));
+  // DISAGREE
+  wordObjects.push(new Word(700, -140, 3, 0, "DISAGREE", 3000, 16));
+
+  // Decoy
+  wordObjects.push(new Word(0, -70, 10, 0, "DO", 1050, 0));
+  wordObjects.push(new Word(450, -187, -5, 0, "NOT", 1050, 1));
+  wordObjects.push(new Word(990, 10, -10, 0, "KNOW", 800, 2));
+  wordObjects.push(new Word(105, 160, -10, 0, "HOW", 1050, 3));
+  wordObjects.push(new Word(900, -140, -10, 0, "TO", 3000, 4));
+  wordObjects.push(new Word(0, -10, -5, 0, "SURE", 800, 5));
+  wordObjects.push(new Word(1000, 0, -1, 0, "IS", 1050, 6));
+  wordObjects.push(new Word(-8, 70, -10, 0, "IT", 1050, 7));
+  wordObjects.push(new Word(580, 40, -5, 0, "INCORRECT", 3000, 8));
+  wordObjects.push(new Word(700, 110, -1, 0, "UNSURE", 3000, 9));
+  wordObjects.push(new Word(-8, -110, -10, 0, "INSECURE", 800, 10));
+  wordObjects.push(new Word(500, -120, 1, 0, "IMPOSTER", 1050, 11));
+  wordObjects.push(new Word(300, 140, 10, 0, "FRAUD", 1050, 12));
+  wordObjects.push(new Word(1380, 142, -1, 0, "UNFORGIVABLE", 1050, 13));
+  wordObjects.push(new Word(300, -187, 10, 0, "CAN", 1050, 14));
+  wordObjects.push(new Word(800, -50, -1, 0, "LIAR", 3000, 15));
+  wordObjects.push(new Word(700, -160, -5, 0, "DISHONEST", 3000, 16));
+
+}
+
+
+// Start
+//
+// When the user clicks the screen
+// the project starts
+function start() {
+
+  // Setup annyang
+  if (annyang) {
+    annyang.addCommands(speech);
+    annyang.start();
+  }
+  // Hide title screen
+  $titleScreen.hide()
+  // Hide
+  $clearScreen.hide();
+
+
+  // Make box droppable
+  $droppableBox.droppable({
+    drop: function(ev, ui) {
+      // Identifies words in droppable box
+      outputArray.push(ui.draggable[0].textContent);
+      let id = parseInt(ui.draggable[0].id);
+      idArray.push(id);
+      wordObjects[id].textPosition();
+      clearScreen();
+
     }
-    // Hide title screen
-    $titleScreen.hide()
+  })
+  resetDroppableBox();
 
-    // I
-    wordObjects.push(new Word(100, 0, 1, 0, "I", 150, 0));
-    // AM
-    wordObjects.push(new Word(500, -100, -1, 0, "AM", 1050, 1));
-    // // SORRY
-    wordObjects.push(new Word(990, 50, 1, 0, "SORRY", 800, 2));
-    // WAS
-    wordObjects.push(new Word(105, 142, -5, 0, "WAS", 1050, 3));
-    // WRONG
-    wordObjects.push(new Word(900, -187, 5, 0, "WRONG", 3000, 4));
-    // LOVE
-    wordObjects.push(new Word(0, -50, -1, 0, "LOVE", 800, 5));
-    // YOU
-    wordObjects.push(new Word(1000, 0, 5, 0, "YOU", 1050, 6));
-    // NEED
-    wordObjects.push(new Word(-8, 45, -3, 0, "NEED", 1050, 7));
-    // HELP
-    wordObjects.push(new Word(580, 0, 3, 0, "HELP", 3000, 8));
-    // PLEASE
-    wordObjects.push(new Word(700, 160, -3, 0, "PLEASE", 3000, 9));
-    // STOP
-    wordObjects.push(new Word(-8, -187, 1, 0, "STOP", 800, 10));
-    // WANT
-    wordObjects.push(new Word(500, -140, -3, 0, "WANT", 1050, 11));
-    // THIS
-    wordObjects.push(new Word(300, 100, 1, 0, "THIS", 1050, 12));
-    // IN
-    wordObjects.push(new Word(1380, 142, -5, 0, "IN", 1050, 13));
-    // MY
-    wordObjects.push(new Word(300, -187, 5, 0, "MY", 1050, 14));
-    // OPINION
-    wordObjects.push(new Word(800, -50, -3, 0, "OPINION", 3000, 15));
-    // DISAGREE
-    wordObjects.push(new Word(700, -140, 3, 0, "DISAGREE", 3000, 16));
-
-    // Decoy
-    wordObjects.push(new Word(0, -70, 10, 0, "DO", 1050, 0));
-    wordObjects.push(new Word(450, -187, -5, 0, "NOT", 1050, 1));
-    wordObjects.push(new Word(990, 10, -10, 0, "KNOW", 800, 2));
-    wordObjects.push(new Word(105, 160, -10, 0, "HOW", 1050, 3));
-    wordObjects.push(new Word(900, -140, -10, 0, "TO", 3000, 4));
-    wordObjects.push(new Word(0, -10, -5, 0, "SURE", 800, 5));
-    wordObjects.push(new Word(1000, 0, -1, 0, "IS", 1050, 6));
-    wordObjects.push(new Word(-8, 70, -10, 0, "IT", 1050, 7));
-    wordObjects.push(new Word(580, 40, -5, 0, "INCORRECT", 3000, 8));
-    wordObjects.push(new Word(700, 110, -1, 0, "UNSURE", 3000, 9));
-    wordObjects.push(new Word(-8, -110, -10, 0, "INSECURE", 800, 10));
-    wordObjects.push(new Word(500, -120, 1, 0, "IMPOSTER", 1050, 11));
-    wordObjects.push(new Word(300, 140, 10, 0, "FRAUD", 1050, 12));
-    wordObjects.push(new Word(1380, 142, -1, 0, "UNFORGIVABLE", 1050, 13));
-    wordObjects.push(new Word(300, -187, 10, 0, "CAN", 1050, 14));
-    wordObjects.push(new Word(800, -50, -1, 0, "LIAR", 3000, 15));
-    wordObjects.push(new Word(700, -160, -5, 0, "DISHONEST", 3000, 16));
+  // Make words draggable
+  $(".draggableBox").draggable({
+    //start..
+    start: function(event, ui) {},
+    //stop..
+    stop: function(event, ui) {},
+    drag: function(event, ui) {}
+  });
 
 
-    // Make box droppable
-    $droppableBox.droppable({
-      drop: function(ev, ui) {
-        // Identifies words in droppable box
-        outputArray.push(ui.draggable[0].textContent);
-        let id = parseInt(ui.draggable[0].id);
-        idArray.push(id);
-        wordObjects[id].textPosition();
-        clearScreen();
-      }
-    })
+  // Loop
+  //
+  // Animation for words
+  function loop() {
 
-    // Make words draggable
-    $(".draggableBox").draggable({
-      //start..
-      start: function(event, ui) {},
-      //stop..
-      stop: function(event, ui) {},
-      drag: function(event, ui) {}
-    });
-
-
-    // Loop
-    //
-    // Animation for words
-    function loop() {
-
-      for (let i = 0; i < wordObjects.length; i++) {
-        wordObjects[i].move();
-      }
-      requestAnimationFrame(loop);
+    for (let i = 0; i < wordObjects.length; i++) {
+      wordObjects[i].move();
     }
     requestAnimationFrame(loop);
   }
-
+  requestAnimationFrame(loop);
 }
 
 
@@ -226,9 +214,22 @@ function clearScreen() {
 
     // If a sentence is not recognised,
   } else if (outputArray.length >= 3) {
-    for (let i = 0; i < idArray.length; i++) {
-      wordObjects[idArray[i]].reset();
-    }
+    resetDroppableBox();
+  }
+  // Reset when screen is cleared
+  $clearScreen.click(function() {
+    start();
+  });
+}
+
+
+// Reset Droppable Box
+//
+// Everything in droppable box is reset
+// and speed increases
+function resetDroppableBox() {
+  for (let i = 0; i < idArray.length; i++) {
+    wordObjects[idArray[i]].reset();
     // the project resets
     idArray = [];
     outputArray = [];
@@ -245,39 +246,55 @@ function clearScreen() {
 function iAmSorrySaid() {
   $sentenceDisplayed.text("I AM SORRY");
   $clearScreen.fadeIn('slow');
+  // Reset when screen is cleared
+  $clearScreen.click(start);
 }
 
 function iWasWrongSaid() {
   $sentenceDisplayed.text("I WAS WRONG");
   $clearScreen.fadeIn('slow');
+  // Reset when screen is cleared
+  $clearScreen.click(start);
 }
 
 function iLoveYouSaid() {
   $sentenceDisplayed.text("I LOVE YOU");
   $clearScreen.fadeIn('slow');
+  // Reset when screen is cleared
+  $clearScreen.click(start);
 }
 
 function iNeedHelpSaid() {
   $sentenceDisplayed.text("I NEED HELP");
   $clearScreen.fadeIn('slow');
+  // Reset when screen is cleared
+  $clearScreen.click(start);
 }
 
 function pleaseStopSaid() {
   $sentenceDisplayed.text("PLEASE STOP");
   $clearScreen.fadeIn('slow');
+  // Reset when screen is cleared
+  $clearScreen.click(start);
 }
 
 function iWantThisSaid() {
   $sentenceDisplayed.text("I WANT THIS");
   $clearScreen.fadeIn('slow');
+  // Reset when screen is cleared
+  $clearScreen.click(start);
 }
 
 function inMyOpinionSaid() {
   $sentenceDisplayed.text("IN MY OPINION");
   $clearScreen.fadeIn('slow');
+  // Reset when screen is cleared
+  $clearScreen.click(start);
 }
 
 function iDisagreeSaid() {
   $sentenceDisplayed.text("I DISAGREE");
   $clearScreen.fadeIn('slow');
+  // Reset when screen is cleared
+  $clearScreen.click(start);
 }
